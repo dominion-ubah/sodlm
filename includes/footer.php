6cb89c63ad3ff522a7aa2a3e5643a2f794b82edc
domee_u <domee.ubah@gmail.com>
2017-04-22 10:13:42 -0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,81 @@
+<?php
+
+?>
+
+ <section id="low">
+			<section class="container">
+			<nav style=" padding-right: :3em; widtH: 32em;">
+			     <ul>
+						<li>Audio</li>
+						<li><a href="#">Weekly Teachings</a></li>
+						<li><a href="#">Free Downloads</a></li>
+						<li><a href="#">Podcasts</a></li>
+						<li><a href="#">Event Mp3 Archives</a></li>
+						<li><a href="#">Conference Teachings</a></li>
+						
+			     </ul>
+			     
+			     <ul>
+						<li>Reading</li>
+						<li><a href="#">Teaching Articles</a></li>
+						<li><a href="#">Daily Devotionals</a></li>
+						<li><a href="#">Camp Meeting</a></li>
+						<li><a href="#">Guestbook</a></li>
+						<li><a href="#">Online Magazine</a></li>
+						</ul>
+			</nav>
+                       <article class="form1"w>
+                                <h4>Subscribe to SODLM</h4>
+                                <p></p>
+                                <form >
+                                        <input type="text" placeholder="Name" name="sub-name"/>
+                                        <input type="text" placeholder="Surname" name="sub-surname"/>
+                                        <input type="email" placeholder="Email" name="sub-email"/>
+                                        <input type="checkbox" name="chkbx"/>
+                                        
+                                        <input type="submit"  value="Subscribe" name="sbm"/>
+                                </form>
+                        </article>
+                        <nav style=" padding-left:4em; widtH: 32em;"><ul >
+						<li>About Us</li>
+						<li><a href="#">About SODLM</a></li>
+						<li><a href="#">Ministry History</a></li>
+						<li><a href="#">Outreach</a></li>
+						<li><a href="#">School of BASICS</a></li>
+						<li><a href="#">Dorcas Alternative</a></li>
+						<li><a href="#">Statement of Faith</a></li>
+						</ul>
+			<ul>
+						<li>Give</li>
+						<li><a href="#">Partnership</a></li>
+						<li><a href="#">Kingdom Investment</a></li>
+						<li><a href="#">Seeds and Pledges</a></li>
+						<li><a href="#">Donate</a></li>
+						</ul>
+			</nav>
+		</section>
+                </section>
+	<section class="container">
+                <footer>
+                    &copy; 2016 All Right Reserved.
+                    <span>Streams of Divine Life Ministries</span>
+                </footer>
+        
+                            </section>
+
+
+                            <script type="text/javascript">
+                            	
+                            	$('.carousel').carousel({
+  interval: 2000
+})
+                 
+
+
+                            </script>
+				            
+                            
+        </body>
+
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,300 @@
+<?php
+	
+?>
+
+<!DOCTYPE html>
+<html>
+        <head>
+                
+            <title>
+                    Streams of Divine Life
+            </title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<link href="css/style2.css" rel="stylesheet" type="text/css"/>
+            <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+            
+            <link href="css/font-awesome.css" rel="stylesheet" type="text/css"/>
+            <script type="text/javascript" src="js/jquery.js"></script>
+                            <script type="text/javascript" src="js/bootstrap.js"></script>
+       
+        </head>
+
+        <body>
+                <section id="haed">
+                                    <ul>
+                                            <li><a class="" data-toggle="modal" data-target="#myModallog" href="#">Login</a></li>
+
+                                            <!--   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> -->
+<div class="modal fade" id="myModallog" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Login</h4>
+      </div>
+      <div class="modal-body">
+      <style type="text/css">
+      	
+      	.log-cont{width: 80%; border:1px solid red; margin:0 auto; padding: 2em;}
+      	.log-cont input, .log-cont label{ width: 95%; color: blue; font-style:italic; }
+      </style>
+      <div class="log-cont form-group">
+
+        <label>Username</label><input class="form-control" type="text" name="user_id">
+        <label>Password</label><input class="form-control" type="password" name="user_password">
+
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Leave</button>
+        <button type="submit" class="btn btn-primary">Enter</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<!--    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< -->
+
+
+                                                        <li><a href="#" data-toggle="modal" data-target="#myModalacct">Create Account</a></li>
+
+    <!--   >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> -->
+<div class="modal fade" id="myModalacct" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Create Account</h4>
+      </div>
+      <div class="modal-body">
+      <style type="text/css">
+      	
+      	.log-cont{width: 80%; margin:0 auto; padding: 2em;}
+      	.log-cont input, .log-cont label{ width: 95%; color: blue; font-style:italic; }
+      </style>
+      <div class="log-cont form-group">
+
+        <label>Name</label><input class="form-control" type="text" name="user_id">
+        <label>Telephone</label><input class="form-control" type="password" name="user_password">
+
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Reset</button>
+        <button type="submit" class="btn btn-primary">Register</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<!--    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< -->
+                                    </ul>
+                            </section>
+                <section class="container">
+                <hgroup>
+                    
+                            
+                        
+                    <header>
+                            <div class="logo"><img src="css/img/logo.png"/></div>
+                            <h3>
+                                streams of divine life ministries
+                            </h3>
+
+                            <nav class="nav1">
+                        <ul>		<li><input type="text" name="search" placeholder="Search the site" /></li>
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="contact.php">Contact Us</a></li>
+                                    <li><a href="about.php">About SODLM</a></li>
+                                    <li><a href="#">Ministry Blog</a></li>
+                                    <li><a href="guest-bk.php">Guest Book</a></li>
+                                    <li><a href="store.php"><span style="color:red;" class="glyphicon glyphicon-shopping-cart" aria-hidden="true">.</span>Store</a></li>
+                                    
+                            </ul>
+                    </nav>
+                    <nav>
+                           <ul>
+                                    <li><a href="#"><div class="fb"></div></a></li>
+                                    <li><a href="#"><div class="ig"></div></a></li>
+                                    <li><a href="#"><div class="tw"></div></a></li>
+                                    <li><a href="#"><div class="pi"></div></a></li>
+                                    <li><a href="#"><div class="gp"></div></a></li>
+                                     <!--<li><a href="#"><div class="yt"></div></a></li>-->
+                                    
+                            </ul> 
+                    </nav>
+
+                    </header>
+                    <nav>
+                          <ul id="main-nav">
+                                    <li><span style="color:red; float: left;" class="glyphicon glyphicon-chevron-down" aria-hidden="true">  </span><a href="#" style=" float: left;">Reading</a>
+						<div class="dropee drread">
+								<div>
+									<ul>
+									<li><a href="#">Teachings</a></li>
+									<li><a href="#">Daily Devotionals</a></li>
+									<li><a href="#">Sermons</a></li>
+									</ul>
+									<hr style="margin: 0 5em; border-color: yellow;" />
+									<ul>
+									<li><a href="#">Guest Book</a></li>
+								<li><a href="#">Magazine</a></li>
+								<li><a href="#">Life Connect</a></li>
+								</ul>
+								</div>
+						
+
+<div>
+									<aside><img src="css/img/articles.ico"><h5>Articles</h5></aside>
+									<aside><img src="css/img/notes.png"><h5>Notes</h5></aside>
+									<aside><img src="css/img/guestbk.ico"><h5>Guest Book</h5></aside>
+									<aside ><img src="css/img/sermons.ico"><h5>Sermons</h5></aside>
+									
+								</div>
+								<div>
+								<aside></aside>
+								<aside></aside>
+								</div> </div>
+							
+					</li>
+
+                                    <li><span style="color:red; float: left;" class="glyphicon glyphicon-chevron-down" aria-hidden="true">  </span><a href="#" style=" float: left;">Audio</a>
+							<div class="dropee draudio">
+								<div>
+									<ul>
+									<li><a href="#">Teachings</a></li>
+									<li><a href="#">Conference</a></li>
+									<li><a href="#">Life Connect</a></li>
+									</ul>
+									<hr style="margin: 0 5em; border-color: yellow;" />
+									<ul>
+									<li><a href="#">Podcasts</a></li>
+								<li><a href="#">Live Streaming</a></li>
+								<li><a href="#">Notes</a></li>
+								</ul>
+								</div>
+						
+
+<div>
+									<aside><img src="css/img/sermon1.ico"><h5>Sermons</h5></aside>
+									<aside><img src="css/img/mic2.ico"><h5>Teachings</h5></aside>
+									<aside><img src="css/img/pod.ico"><h5>Podcasts</h5></aside>
+									<aside ><img src="css/img/speak.ico"><h5>Life Connect</h5></aside>
+								</div>
+								<div>
+								<aside></aside>
+								<aside></aside>
+								</div> </div></li>
+                                    <li><span style="color:red; float: left;" class="glyphicon glyphicon-chevron-down" aria-hidden="true">  </span><a href="#" style=" float: left;">Video</a>
+							<div class="dropee drvideo">
+								<div>
+									<ul>
+									<li><a href="#">Sermon</a></li>
+									<li><a href="#">Testimonies</a></li>
+									<li><a href="#">Teaching</a></li>
+									</ul>
+									<hr style="margin: 0 5em; border-color: yellow;" />
+									<ul>
+									<li><a href="#">Life TV</a></li>
+								<li><a href="#">Your Word Today</a></li>
+								<li><a href="#">Grace Unlimited</a></li>
+								</ul>
+								</div>
+						
+
+<div>
+									<aside><img src="css/img/mov.ico"><h5>Life TV</h5></aside>
+									<aside><img src="css/img/dvd.ico"><h5>Teachings</h5></aside>
+									<aside><img src="css/img/guestbk.ico"><h5>Testimoney</h5></aside>
+									<aside ><img src="css/img/ear.ico"><h5>Your Word Today</h5></aside>
+								</div>
+								<div>
+								<aside></aside>
+								<aside></aside>
+								</div> </div></li>
+                                    <li><span style="color:red; float: left;" class="glyphicon glyphicon-chevron-down" aria-hidden="true">  </span><a href="#" style=" float: left;">Events</a>
+								<div class="dropee drevent">
+								<div>
+										<h4>Upcoming Events</h4>
+									<ul class="main-event">
+									<li><a href="#"></a></li>
+									<li><a href="#">Teaching</a></li>
+									<li><a href="#">Teaching</a></li>
+									<li><a href="#">Teaching</a></li>
+									<li><a href="#">Teaching</a></li>
+								<li><a href="#">Teaching</a></li>
+								<li><a href="#">Teaching</a></li>
+								</ul>
+								</div>
+						
+
+<div>
+									<aside><img src="css/img/calmak.ico"></aside>
+									<aside><img src="css/img/note.png"></aside>
+									<aside><img src="css/img/date.ico"></aside>
+									<aside ><img src="css/img/cal.ico"></aside>
+								</div>
+								<div>
+								<aside></aside>
+								<aside></aside>
+								</div> </div></li>
+                                    <li><span style="color:red; float: left;" class="glyphicon glyphicon-chevron-down" aria-hidden="true">  </span><a href="#" style=" float: left;">Ministry FOCUS</a>
+								<div class="dropee drfocus">
+								<div>
+									<ul>
+									<li><a href="#">Teaching</a></li>
+									<li><a href="#">Teaching</a></li>
+									<li><a href="#">Teaching</a></li>
+									</ul>
+									<hr style="margin: 0 5em; border-color: yellow;" />
+									<ul>
+									<li><a href="#">Teaching</a></li>
+								<li><a href="#">Teaching</a></li>
+								<li><a href="#">Teaching</a></li>
+								</ul>
+								</div>
+						
+
+<div>
+									<aside><img src="css/img/part1.ico"></aside>
+									<aside><img src="css/img/high.ico"></aside>
+									<aside><img src="css/img/work.ico"></aside>
+									<aside ><img src="css/img/coin.ico"></aside>
+								</div>
+								<div>
+								<aside></aside>
+								<aside></aside>
+								</div> </div></li>
+                                    <li><span style="color:red; float: left;" class="glyphicon glyphicon-chevron-down" aria-hidden="true">  </span><a href="#" style=" float: left;">Resource Center</a>
+								<div class="dropee drresource">
+								<div>
+									<ul>
+									<li><a href="#">Teaching</a></li>
+									<li><a href="#">Teaching</a></li>
+									<li><a href="#">Teaching</a></li>
+									</ul>
+									<ul>
+									<li><a href="#">Teaching</a></li>
+								<li><a href="#">Teaching</a></li>
+								<li><a href="#">Teaching</a></li>
+								</ul>
+								</div>
+						
+
+<div>
+									<aside><img src="css/img/pack.ico"></aside>
+									<aside><img src="css/img/dvdb.ico"></aside>
+									<aside><img src="css/img/cdb.ico"></aside>
+									<aside ><img src="css/img/spirit.ico"></aside>
+								</div>
+								<div>
+								<aside></aside>
+								<aside></aside>
+								</div> </div></li>
+                            </ul>
+			</nav>
+                    
+                </hgroup>
+                </section>
